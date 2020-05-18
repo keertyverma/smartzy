cfd697bddca6abad72637c75706836fc92ad1efb
Keerty Verma <keertyverma1303@gmail.com>
2020-05-18 19:51:35 +0530

    configured api server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express"),
+    cors = require("cors");
+
+require("dotenv").config();
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// cross - origin resource sharing
+app.use(cors());
+app.use(express.json())
+
+app.use("/", function (req, res) {
+    res.send("Hi");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+})
