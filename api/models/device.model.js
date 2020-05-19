710daf585a8c210c2ddf63f1572088af29f703a3
Keerty Verma <keertyverma1303@gmail.com>
2020-05-19 17:35:01 +0530

    CRUD operation on device API

--- api/models/device.model.js
+++ api/models/device.model.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const deviceSchema = new Schema({
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     type: { type: String, required: true },
-    ip: { type: String, required: true },
-    mac: { type: String, required: true },
-});
+}, { _id: false });
 
 const Device = mongoose.model('Device', deviceSchema, "device");
 
